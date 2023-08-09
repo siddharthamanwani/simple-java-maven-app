a723d9a5f942568bd69304a6c5f9790c129920a8
siddharthamanwani <sidroy117@gmail.com>
2023-08-09 17:56:33 +0530

    Update script.dev.groovy

--- jenkins/script.dev.groovy
+++ jenkins/script.dev.groovy
@@ -2,7 +2,7 @@ def load_check() {
     echo 'Script Loaded-----------------------'
 }
 
-def print_message(integer loop) {
+def print_message(int loop) {
    def i =0
   for (i=0 ; i< loop; i ++) {
       echo ' number is ${i} \n'
