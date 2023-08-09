4037cb85e84995f08562f2f052208a96ad18a9de
siddharthamanwani <sidroy117@gmail.com>
2023-08-09 17:49:30 +0530

    Create script.dev.groovy

---
+++ jenkins/script.dev.groovy
@@ -0,0 +1,10 @@
+def load_check() {
+    echo 'Script Loaded-----------------------'
+}
+
+def print_message(integer loop) {
+   def i =0
+  for (i=0 ; i< loop; i ++) {
+      echo ' number is ${i} \n'
+  }
+}
